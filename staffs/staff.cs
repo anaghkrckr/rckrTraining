b37869351c444f78f52fe457db5d5e36813fc5e6
anaghkrckr <anaghk.off@gmail.com>
2021-08-13 12:30:40 +0530

    naming conention, added getters and setters

--- staffs/staff.cs
+++ staffs/staff.cs
@@ -1,12 +1,43 @@
 using System;
 
 namespace training{
+    public class Staff{
+        private int staffId=0;
+        private string staffName;
+        private int staffAge;
 
-    abstract class Staff{
-        public abstract void add();
-        public abstract void delete();
-        public abstract void view();
-        public abstract void update();
+        public int StaffId{
+            get{return staffId;}
+            set{staffId=value;}
+        }
+        public string StaffName{
+            get{return staffName;}
+            set{staffName=value;}
+        }
+        public int StaffAge{
+            get{return staffAge;}
+            set{staffAge=value;}
+        }
+
+
+
+
+
+
+
+        public virtual void AddStaff(){
+            Console.WriteLine("Name:");
+            this.StaffName=Console.ReadLine();
+            Console.WriteLine("Age:");
+            this.StaffAge=Convert.ToInt32(Console.ReadLine());
+
+        }
+        public virtual void ViewStaff(){}
+        // public abstract void delete();
+        public virtual void UpdateStaff(){}
+
+
+      
 
     }
 
--- staffs/support.cs
+++ staffs/support.cs
@@ -1,11 +1,56 @@
 using System;
 
 namespace training{
-    class Support :Staff{
-        public override void add(){
+    public class Support :Staff{
+        private string supportDepartment;
+
+        public string SupportDepartment{
+            get{return supportDepartment;}
+            set{supportDepartment=value;}
+        }
+
+
+        public override void AddStaff(){
+            base.AddStaff();
+            Console.WriteLine("Department:");
+            this.supportDepartment=Console.ReadLine();
+            this.StaffId++;
+            Console.WriteLine("StaffId\tName\tAge\tDepartment");
+            Console.WriteLine(this.StaffId+"\t"+this.StaffName+"\t"+this.StaffAge+"\t"+this.SupportDepartment);
+        }
+         public override void ViewStaff(){
+            Console.WriteLine("SUPPORT");
+            Console.WriteLine("StaffId\tName\tAge\tDepartment");
+            Console.WriteLine(this.StaffId+"\t"+this.StaffName+"\t"+this.StaffAge+"\t"+this.SupportDepartment);
 
         Console.WriteLine("Support");
         }
+
+        public override void UpdateStaff(){
+            int UpdateOptions;
+            bool loop=true;
+            do{
+                Console.WriteLine("Select value to update\n1)Name\n2)Department\n3)Go back\n");
+                UpdateOptions=Convert.ToInt32(Console.ReadLine());
+                switch(UpdateOptions){
+                case 1:
+                    Console.WriteLine("Enter new Name:");
+                    this.StaffName=Console.ReadLine();;
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Department:");
+                    this.SupportDepartment=Console.ReadLine();;
+                    break;
+                case 3:
+                    loop=false;
+                    break;
+                }
+
+            }while(loop);
+
+        }
+
+
     }
 
 }
