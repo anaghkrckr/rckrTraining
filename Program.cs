2e03fea9aae379e2aa6bb2e17c6a2ddcc4955292
Anaghkrckr <anaghk.off@outlook.com>
2021-08-16 15:14:58 +0530

    added multi object support

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace training
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        public static List<Teacher> teacherList = new List<Teacher>();
+        public static List<Administrator> administratorList = new List<Administrator>();
+        public static List<Support> supportList = new List<Support>();
+
+        static void Main(string[] args){
             Program.SelectStaff();    
         }
 
@@ -15,50 +20,56 @@ namespace training
             do{
                 Console.WriteLine("Select the type of staff\n1)Teacher\n2)Administrator\n3)Support\n4)QUIT");
                 staffOptions=Convert.ToInt32(Console.ReadLine());
-                switch(staffOptions){
-                    case 1: 
-                        Staff teacher=new Teacher();
-                        Program.MainActions(teacher);
-                        break;
-                    case 2: 
-                        Staff administrator=new Administrator() ;
-                        Program.MainActions(administrator);
-                        break;
-                    case 3: 
-                        Staff support=new Support() ;
-                        Program.MainActions(support);
-                        break;
-                    case 4: 
-                        continueStaffSelection=false;
-                        break;
-                }
+               
+                    switch(staffOptions){
+                        case 1: 
+                            Program.MainActions("Teacher");
+                            break;
+                        case 2: 
+                            Program.MainActions("Administrator");
+                            break;
+                        case 3: 
+                            Program.MainActions("Support");
+                            break;
+                        case 4:
+                            continueStaffSelection=false;
+                            break;
+                        }
             }while(continueStaffSelection);
         }
 
-        private static void MainActions(Staff staff){
+
+        private static void MainActions(String staffType){
             int mainOptions;
-            bool continueMainActions=true;
-            string staffType=staff.GetType().Name.ToUpper();
+            bool continueMainActions = true;
             do{
-                Console.WriteLine("WLECOME TO {0} STAFF PORTAL\nselect Options\n1)Add staff\n2)Delete staff\n3)Update staff details\n4)View staff details",staffType);
-                mainOptions=Convert.ToInt32(Console.ReadLine());
-                switch(mainOptions){
-                    case 1: 
-                        staff.AddStaff();
+                Console.WriteLine("WLECOME TO {0} STAFF PORTAL\nselect Options\n1)Add staff\n2)Delete staff\n3)Update staff details\n4)View staff details", staffType);
+                mainOptions = Convert.ToInt32(Console.ReadLine());
+                switch (mainOptions){
+                    case 1:
+                        Staff.StaffAdd(staffType);
                         break;
-                    case 2: 
-                        // staff.DeleteStaff();
+                    case 2:
+                        Staff.DeleteStaff(staffType);
                         break;
-                    case 3: 
-                        staff.UpdateStaff();
+                    case 3:
+                        Staff.UpdateStaff(staffType);
                         break;
-                    case 4: 
-                       staff.ViewStaff();
+                    case 4:
+                        Staff.StaffView(staffType);
                         break;
-                    }
+                }
                 Console.WriteLine("Do you want to continue?(0/1):");
-                continueMainActions=Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
-            }while(continueMainActions);
+                continueMainActions = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+            } while (continueMainActions);
         }
+
+
+        
+
+        
+
+
+        
     }  
 }
