9365a38385ddb3223ebe511ef0063bac340985c9
Anaghkrckr <anaghk.off@outlook.com>
2021-08-27 21:07:07 +0530

    feat:added stored procedure for database management

---
+++ dbo/User Defined Types/Staffs.sql
@@ -0,0 +1,8 @@
+﻿CREATE TYPE [dbo].[Staffs] AS TABLE (
+    [StaffName]       NVARCHAR (20) NULL,
+    [StaffAge]        INT          NULL,
+    [StaffType]       VARCHAR (20) NULL,
+    [StaffDepartment] VARCHAR (30) NULL);
+
+
+
