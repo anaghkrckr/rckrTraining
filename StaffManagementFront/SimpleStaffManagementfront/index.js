f796b646f29fccb296f3cbd4bbe17743230439ba
Anaghkrckr <anaghk.off@outlook.com>
2021-09-12 20:19:05 +0530

    fix: minor bug fixes in sorting

--- StaffManagementFront/SimpleStaffManagementfront/index.js
+++ StaffManagementFront/SimpleStaffManagementfront/index.js
@@ -4,11 +4,30 @@ let deleteList = []
 let mainStaffList = []
 let pagedStaffList = []
 let staffTypes = {
-    Teacher: "Subject",
-    Administrator: "Administrator Department",
-    Support: "Support Department"
+    staffs: {
+        All: { objName: "department", name: "Department" },
+        Teacher: { objName: "subject", name: "Subject" },
+        Administrator: { objName: "administratorDepartment", name: "Administrator Department" },
+        Support: { objName: "supportDepartment", name: "Support Department" }
+    },
+    selected: "All"
 }
-let tableHead = { staffId: "Id", staffName: "Name", staffAge: "Age", staffType: "Type", department: "Department" }
+
+
+let sortOrder = { Id: 0, Name: 0, Age: 0, Type: 0, }
+
+let icons = {
+    sortUp: "<i class='bi bi-caret-down-fill'></i>",
+    sortDown: "<i class='bi bi-caret-up-fill'></i>"
+}
+
+let tableHead = {
+    staffId: "Id",
+    staffName: "Name",
+    staffAge: "Age",
+    staffType: "Type",
+}
+
 
 let currentPage = 0;
 
@@ -19,27 +38,28 @@ const form = document.querySelector('form');
 form.addEventListener('submit', formSubmitHandler);
 
 const staffOptions = document.getElementById("staffs")
-staffOptions.addEventListener("change", () => handleMainStaffListChange(staffOptions))
 
 main();
 
-
 async function main() {
-    Object.keys(staffTypes).forEach((key) => {
-        var option = document.createElement("OPTION");
-        option.textContent = key;
-        option.value = key;
-        staffOptions.appendChild(option);
+    Object.keys(staffTypes.staffs).forEach((key) => {
+        var a = document.createElement("a");
+        a.onclick = () => {
+            fetchStaff(key);
+            currentPage = 0;
+        }
+        a.href = "#"
+        a.textContent = key;
+        staffOptions.appendChild(a);
     })
 
-    let staffType = staffOptions.value;
-    fetchStaff(base_url, staffType)
+    fetchStaff(staffTypes.selected)
 }
-
-
-async function fetchStaff(url, staffType) {
-    let fetchUrl = url;
-    if (staffOptions.value != "All") {
+async function fetchStaff(staffType) {
+    staffTypes.selected = staffType;
+    document.getElementById("staffSelectionBtn").textContent = staffTypes.selected;
+    let fetchUrl = base_url;
+    if (staffTypes.selected != "All") {
         fetchUrl = base_url + "/" + staffType;
         staffOptions.value = staffType;
     }
@@ -47,67 +67,78 @@ async function fetchStaff(url, staffType) {
     let response = await fetch(fetchUrl)
     var data = await response.json();
     mainStaffList = data;
-    rednderTableHelper(data)
+    sortOrder[staffTypes.staffs[staffTypes.selected].objName] = 0;
+    renderStaffTable()
 }
 
-function rednderTableHelper(data) {
-    pagedStaffList = paginate(data)
-    if (currentPage >= pagedStaffList.length && pagedStaffList.length > 0) {
-        currentPage = pagedStaffList.length - 1;
+function sortStaffList(item) {
+    sortOrder[item] = !sortOrder[item];
+    if (item == "department") {
+        mainStaffList.sort(function(a, b) {
+            return sortOrder[item] ? (((b.subject || b.administratorDepartment || b.supportDepartment) > (a.subject || a.administratorDepartment || a.supportDepartment)) ? 1 : (((a.subject || a.administratorDepartment || a.supportDepartment) > (b.subject || b.administratorDepartment || b.supportDepartment)) ? -1 : 0)) : (((a.subject || a.administratorDepartment || a.supportDepartment) > (b.subject || b.administratorDepartment || b.supportDepartment)) ? 1 : (((b.subject || b.administratorDepartment || b.supportDepartment) > (a.subject || a.administratorDepartment || a.supportDepartment)) ? -1 : 0));
+        });
+    } else {
+        mainStaffList.sort(function(a, b) {
+            return sortOrder[item] ? ((b[item] > a[item]) ? 1 : ((a[item] > b[item]) ? -1 : 0)) : ((a[item] > b[item]) ? 1 : ((b[item] > a[item]) ? -1 : 0));
+        });
     }
-    renderStaffTable(pagedStaffList, currentPage);
+    renderStaffTable()
 }
 
-
-function sortStaffList(staffList, item) {
-    staffList.sort(function(a, b) { return (a[item] > b[item]) ? 1 : ((b[item] > a[item]) ? -1 : 0); });
-    rednderTableHelper(staffList);
-}
-
-function renderStaffTable(data, pageNumber) {
-    var headItems = tableHead;
+function renderStaffTable() {
+    pagedStaffList = paginate(mainStaffList)
+    var headItems = {...tableHead };
     let table = document.getElementById("staffTable");
-    table.innerHTML = ""
-    let departmentName = staffTypes[staffOptions.value] ? staffTypes[staffOptions.value] : "Department";
+    table.getElementsByTagName('tbody')[0].innerHTML = ""
+    table.getElementsByTagName('thead')[0].innerHTML = ""
+    let department = staffTypes.staffs[staffTypes.selected];
     var header = table.createTHead();
     var row = header.insertRow(0);
-    headItems.department = departmentName;
+    headItems[department.objName] = department.name;
     Object.keys(headItems).forEach((key) => {
         var cell = row.insertCell(-1)
-        cell.innerHTML = `<b>` + headItems[key];
+        cell.innerHTML = (sortOrder[key] ? icons.sortUp : icons.sortDown) + `<b>` + headItems[key];
         cell.onclick = () =>
-            sortStaffList(mainStaffList, key)
+            sortStaffList(key)
+        cell.style.cursor = "pointer"
     })
+
+
     row.insertCell(-1).innerHTML = `<b>Delete`;
     const staffPages = document.getElementById("staffPAgesList")
     staffPages.innerHTML = ""
-    if (data[pageNumber] == null) {
+    if (pagedStaffList.length == 0) {
         return;
     }
-    data[pageNumber].forEach(staff => {
-        var row = table.insertRow(-1);
+
+    pagedStaffList[currentPage].forEach(staff => {
+        var row = table.getElementsByTagName('tbody')[0].insertRow(-1);
         row.onclick = function() {
-            updateForm(staff)
+            updateStaffForm(staff)
         }
+        row.style.cursor = "pointer"
         Object.keys(staff).forEach(function(key, i) {
-            row.insertCell(i).innerHTML = staff[key]
+            var cell = row.insertCell(i)
+            cell.innerHTML = staff[key]
+            cell.className = "cell"
+            cell.style.width = "300px"
         })
-        var but = document.createElement("button");
-        but.className = "btn btn-danger";
-        but.textContent = "DELETE";
-        but.onclick = function() {
+        var deleteStaffButton = document.createElement("button");
+        deleteStaffButton.className = "btn btn-danger";
+        deleteStaffButton.textContent = "DELETE";
+        deleteStaffButton.onclick = function() {
             delStaffHelper(staff)
         };
-        row.insertCell(-1).appendChild(but);
-        var check = document.createElement("input")
-        check.type = "checkbox";
-        check.className = "form-check-input";
-        check.id = staff.staffId;
-        check.onclick = () => {
+        row.insertCell(-1).appendChild(deleteStaffButton);
+        var deleteStaffCheckBox = document.createElement("input")
+        deleteStaffCheckBox.type = "checkbox";
+        deleteStaffCheckBox.className = "form-check-input";
+        deleteStaffCheckBox.id = staff.staffId;
+        deleteStaffCheckBox.onclick = () => {
             event.stopImmediatePropagation()
             deleteList.push(staff);
         }
-        row.insertCell(-1).appendChild(check);
+        row.insertCell(-1).appendChild(deleteStaffCheckBox);
     })
 
 
@@ -115,26 +146,25 @@ function renderStaffTable(data, pageNumber) {
         var li = document.createElement("li");
         li.className = "page-link";
         var id = "page-" + (i + 1)
-
         li.id = id;
+        li.style.cursor = "pointer"
         li.onclick = () => {
             currentPage = i
             renderStaffTable(pagedStaffList, currentPage);
-            document.getElementById(id).style.backgroundColor = "red"
         }
         li.textContent = i + 1
         staffPages.appendChild(li)
-
     })
-    var pageId = "page-" + (pageNumber + 1)
-    document.getElementById(pageId).style.backgroundColor = "red"
+
+    var pageId = "page-" + (currentPage + 1)
+    document.getElementById(pageId).style.backgroundColor = "#00a8ff"
 }
 
 
 function formSubmitHandler() {
     let method = "POST"
     event.preventDefault();
-    staff = {}
+    let staff = {}
     let url = base_url;
     const data = new FormData(event.target);
     staff.staffName = data.get('staffName');
@@ -145,18 +175,8 @@ function formSubmitHandler() {
         method = "PUT"
         url = base_url + "/" + staff.staffType + "/" + staff.staffId
     }
-
-    switch (staff.staffType.toLowerCase()) {
-        case "teacher":
-            staff.subject = data.get('staffDepartment');
-            break;
-        case "administrator":
-            staff.administratorDepartment = data.get('staffDepartment');
-            break;
-        case "support":
-            staff.supportDepartment = data.get('staffDepartment');
-            break;
-    }
+    let department = staffTypes.staffs[staff.staffType].objName
+    staff[department] = data.get('staffDepartment')
 
     fetch(url, {
             method: method,
@@ -167,7 +187,7 @@ function formSubmitHandler() {
         })
         .then(response => response.json())
         .then(data => {
-            fetchStaff(base_url, staff.staffType);
+            fetchStaff(staffTypes.selected);
             statusPopup(data.status, "#27ae60")
             closeForm();
         })
@@ -178,10 +198,10 @@ function formSubmitHandler() {
 
 function addForm() {
     document.getElementById("staffFormType").innerHTML = "Add Form"
-    openForm(staffOptions.value);
+    openForm(staffTypes.selected);
 }
 
-function updateForm(staff) {
+function updateStaffForm(staff) {
     openForm(staff.staffType);
     document.getElementById("staffFormType").innerHTML = "Update Form"
     if (staff != null) {
@@ -189,8 +209,7 @@ function updateForm(staff) {
         document.getElementById("staffName").value = staff.staffName;
         document.getElementById("staffAge").value = staff.staffAge;
         formStaffLists.value = staff.staffType;
-        document.getElementById("staffDepartment").value = staff.subject || staff.administratorDepartment || staff.supportDepartment;
-
+        document.getElementById("staffDepartment").value = staff.subject || staff.administratorDepartment || staff.supportDepartment
     }
 
 }
@@ -210,32 +229,18 @@ function closeForm() {
 }
 
 function openForm(staffType) {
-    Object.keys(staffTypes).forEach((staffType, i) => {
+    Object.keys(staffTypes.staffs).slice(1).forEach((staffType, i) => {
         var option = document.createElement("OPTION");
         option.textContent = staffType;
         option.value = staffType;
         formStaffLists.appendChild(option);
     });
-    if (staffType != "All") {
-        formStaffLists.value = staffType;
-    }
+    formStaffLists.value = staffType;
     document.getElementById("staffForm").style.display = "block"
     var department = document.getElementById("labelstaffDepartment");
-    if (formStaffLists.value != null) {
-        switch (formStaffLists.value.toLowerCase()) {
-            case "teacher":
-                department.textContent = "Subject";
-                break;
-            case "administrator":
-                department.textContent = "Administrator Department";
-                break;
-            case "support":
-                department.textContent = "Support Department";
-                break;
-        }
-    }
-}
+    department.textContent = staffTypes.staffs[formStaffLists.value].name;
 
+}
 
 function delStaffHelper(staff) {
     event.stopImmediatePropagation()
@@ -273,45 +278,15 @@ async function deleteStaff(staff) {
             deleteList = [];
             statusPopup("Delete Success", "#27ae60")
 
-            fetchStaff(base_url, staffType);
+            fetchStaff(staffTypes.selected);
         })
         .catch((error) => {})
 }
 
-function setConfirmation(value) {
-
-}
-
-function handleMainStaffListChange(staffOptions) {
-
-    if (staffOptions.value != 1) {
-
-        staffType = staffOptions.options[staffOptions.selectedIndex].text;
-        fetchStaff(base_url, staffType);
-    }
-}
-
-
-
-
 function handleFormStaffListChange() {
     var department = document.getElementById("labelstaffDepartment");
     if (formStaffLists.value != null) {
-        switch (formStaffLists.value.toLowerCase()) {
-            case "teacher":
-                department.textContent = "Subject";
-                break;
-            case "administrator":
-                department.textContent = "Administrator Department";
-                break;
-            case "support":
-                department.textContent = "Support Department";
-                break;
-            default:
-                department.textContent = "Department";
-                break;
-        }
-
+        department.textContent = staffTypes.staffs[formStaffLists.value].name.toLowerCase()
     }
 }
 
